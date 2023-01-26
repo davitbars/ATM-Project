6710de7740f191894307a3cde58d15466dc5c344
davitbars <96258965+davitbars@users.noreply.github.com>
2023-01-26 15:40:40 -0800

    Update database.java

--- database.java
+++ database.java
@@ -4,7 +4,7 @@ public class database {
 
     public static Connection getConn(){
         try{
-            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "DavoB2004");
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "PASSWORD");
             return myConn;
         }
         catch(Exception e){
